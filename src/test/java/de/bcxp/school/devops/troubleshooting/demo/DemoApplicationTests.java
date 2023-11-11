3ffdca22b3aad30022a5c941d80d64a74961bd07
Sebastian Breier <sebastian.breier@bcxp.de>
2023-11-11 09:44:01 +0100

    base spring demo app from spring initializer

---
+++ src/test/java/de/bcxp/school/devops/troubleshooting/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bcxp.school.devops.troubleshooting.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
